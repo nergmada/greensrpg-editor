96462139cb4934fc0dff0015824187447216839e
Adam Green <adam.green@kcl.ac.uk>
2021-05-01 22:25:57 +0100

    Fixed date-time issue

--- src/components/DateTime.svelte
+++ src/components/DateTime.svelte
@@ -1,7 +1,8 @@
 <script>
     export let date = new Date();
     let dt = new Date(date * 1000).toISOString().split('T')[0];
-    let t = `${new Date(date * 1000).getUTCHours()}:${new Date(date * 1000).getUTCMinutes()}`;
+    let t = `${new Date(date * 1000).getUTCHours() < 10 ? '0' : ''}${new Date(date * 1000).getUTCHours()}:${new Date(date * 1000).getUTCMinutes() < 10 ? '0' : ''}${new Date(date * 1000).getUTCMinutes()}`;
+    $: console.log(t);
     $: {
         date = Date.parse(`${dt}T${t}:00`)/1000;
     }
